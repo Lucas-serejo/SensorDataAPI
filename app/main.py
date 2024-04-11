1d3d82496a643ca7e3cac47bdcbe5fb4dfd2e613
Lucas Serejo <lucasj.serejo11@gmail.com>
2024-04-11 10:07:26 -0300

    REFACTOR:  foi alterada a disposição dos métodos

--- app/main.py
+++ app/main.py
@@ -1,72 +1,31 @@
 from models import db, SensorData
-import pandas as pd
-from flask import Flask, render_template, request, jsonify
-from datetime import datetime
+from flask import Flask, render_template
 
-
+#Cria uma instância do framework flask
 app = Flask(__name__)
 
-
+#Estabelece a conexão com o banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:mysqlroot@localhost/SensorData'
 db.init_app(app)
 
-# Criar todas as tabelas no banco de dados
+#Criar todas as tabelas no banco de dados
 with app.app_context():
     db.create_all()
 
-# Rota para renderizar o formulário HTML
+#Definindo a rota para renderizar o formulário HTML
 @app.route('/')
 def index():
     return render_template('upload.html')
 
-
-
-#Função destinada a receber os payloads em JSON dos sensores
+#Definindo a rota para receber o arquivo JSON
 @app.route('/data', methods=['POST'])
 def receive_data():
-    data = request.json
-    return SensorData.receive_JSON(data)
+    return SensorData.receive_JSON()
 
-
-
-#Função destinada a receber arquivos csv com dados faltantes no banco
+#Definindo a rota para receber o arquivo CSV
 @app.route('/upload', methods=['POST'])
 def upload():
-    try:
-        # Verifica se um arquivo CSV foi enviado
-        if 'file' not in request.files:
-            return "Nenhum arquivo enviado", 400
-        
-        file = request.files['file']
-        
-        # Verifica se o nome do arquivo está vazio
-        if file.filename == '':
-            return "Nome do arquivo vazio", 400
-        
-        # Verifica se o arquivo é um arquivo CSV
-        if file and file.filename.endswith('.csv'):
-            # Lê o arquivo CSV e processa os dados
-            df = pd.read_csv(file)
-            
-            for _, row in df.iterrows():
-                equipmentId = str(row['equipmentId'])
-                timestamp = datetime.strptime(row['timestamp'], "%Y-%m-%dT%H:%M:%S.%f%z")
-                value = round(float(row['value']), 2)
-                
-                # Cria um novo objeto SensorData e o adiciona ao banco de dados
-                sensor_data = SensorData(equipmentId=equipmentId, timestamp=timestamp, value=value)
-                db.session.add(sensor_data)
-            
-            # Commit para salvar os dados no banco de dados
-            db.session.commit()
-            
-            return "Arquivo CSV processado e dados adicionados ao banco de dados com sucesso!", 200
-        
-        else:
-            return "Por favor, envie um arquivo CSV", 400
-    
-    except Exception as e:
-        return f"Erro ao processar o arquivo CSV: {str(e)}", 500
+    return SensorData.receive_CSV()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/models.py
+++ app/models.py
@@ -3,18 +3,22 @@
 from datetime import datetime
 from flask import jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
 
 db = SQLAlchemy()
 
 
-#Representa a tabela do banco de dados
+#Representa a estrutura do banco de dados
 class SensorData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     equipmentId = db.Column(db.String(50), nullable=True)
     timestamp = db.Column(db.DateTime, nullable=True)
     value = db.Column(db.Float(precision=2), nullable=True)
+    
+    #Função para receber o request JSON
+    def receive_JSON():
 
-    def receive_JSON(data):
+        data = request.json
         equipmentId = data['equipmentId']
         timestamp = datetime.strptime(data['timestamp'], "%Y-%m-%dT%H:%M:%S.%f%z")
         value = round(float(data['value']), 2)
@@ -24,7 +28,47 @@ class SensorData(db.Model):
         db.session.add(new_sensor_data)
         db.session.commit()
 
-        return jsonify({'message': 'Dados recebidos!'}), 201
+        return jsonify({'message': 'Data received!'}), 201
+    
+    #Função para receber o request CSV
+    def receive_CSV():
+        try:
+            # Verifica se um arquivo CSV foi enviado
+            if 'file' not in request.files:
+                return "No file sent", 400
+            
+            file = request.files['file']
+            
+            # Verifica se o nome do arquivo está vazio
+            if file.filename == '':
+                return "File name empty", 400
+            
+            # Verifica se o arquivo é um arquivo CSV
+            if file and file.filename.endswith('.csv'):
+                # Lê o arquivo CSV e processa os dados
+                df = pd.read_csv(file)
+                
+                for _, row in df.iterrows():
+                    equipmentId = str(row['equipmentId'])
+                    timestamp = datetime.strptime(row['timestamp'], "%Y-%m-%dT%H:%M:%S.%f%z")
+                    value = round(float(row['value']), 2)
+                    
+                    # Cria um novo objeto SensorData e o adiciona ao banco de dados
+                    sensor_data = SensorData(equipmentId=equipmentId, timestamp=timestamp, value=value)
+                    db.session.add(sensor_data)
+                
+                # Commit para salvar os dados no banco de dados
+                db.session.commit()
+                
+                return "Arquivo CSV processado e dados adicionados ao banco de dados com sucesso!", 200
+            
+            else:
+                return "Por favor, envie um arquivo CSV", 400
+        
+        except Exception as e:
+            return f"Erro ao processar o arquivo CSV: {str(e)}", 500
+
+
 
 
 
