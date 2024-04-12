abce04a774634c8a1f4283554bce6b6421216a82
Lucas Serejo <lucasj.serejo11@gmail.com>
2024-04-12 15:13:45 -0300

    ADD
    Adição de uma função suporte para abertura da página home automaticamente ao rodar o script.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from numpy import record
+import webbrowser
 from models import db, SensorData
 from flask import Flask, render_template, request
 
@@ -33,6 +33,15 @@ def upload_csv():
     if request.method == 'POST':
         return SensorData.receive_CSV()
     return render_template('upload_csv.html')
+#Suporte para abertura da página home ao rodar o script
+def open_browser():
+    base_url = 'http://127.0.0.1:5000/'
+    try:
+        # Tenta abrir o navegador apenas se a URL não estiver aberta
+        webbrowser.get().open(base_url, new=2)
+    except Exception as e:
+        print(f"Erro ao abrir o navegador: {e}")
 
 if __name__ == '__main__':
+    open_browser()
     app.run(debug=True)
