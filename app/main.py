72160a25c99a4fd8ab6a1d489ae5bad2c398f827
Lucas Serejo <lucasj.serejo11@gmail.com>
2024-04-12 01:24:52 -0300

    ADD
    Link feito entre as páginas home e upload_csv

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from models import db, SensorData
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 #Cria uma instância do framework flask
 app = Flask(__name__)
@@ -15,7 +15,7 @@ with app.app_context():
 #Definindo a rota para renderizar o formulário HTML
 @app.route('/')
 def index():
-    return render_template('upload.html')
+    return render_template('home.html')
 
 #Definindo a rota para receber o arquivo JSON
 @app.route('/data', methods=['POST'])
@@ -23,9 +23,11 @@ def receive_data():
     return SensorData.receive_JSON()
 
 #Definindo a rota para receber o arquivo CSV
-@app.route('/upload_csv', methods=['POST'])
+@app.route('/upload_csv', methods=['GET', 'POST'])
 def upload_csv():
-    return SensorData.receive_CSV()
+    if request.method == 'POST':
+        return SensorData.receive_CSV()
+    return render_template('upload_csv.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
