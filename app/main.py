8e3530a0b851bc590d2d63fc0c567d85532e9349
Lucas Serejo <lucasj.serejo11@gmail.com>
2024-04-15 15:11:32 -0300

    REFACTOR
    Melhoria no simulador de registros para preenchimento do banco para que faça acessando a rota especificada.
    
    Melhoria no front das páginas web.

--- app/main.py
+++ app/main.py
@@ -1,72 +1,84 @@
-from datetime import datetime
+from datetime import datetime, timezone
 import os
 import webbrowser
-from matplotlib import pyplot as plt
-# from graphic_generator import *
 from models import db, SensorData
-from flask import Flask, jsonify, render_template, request, send_file
+from flask import Flask, jsonify, render_template, request
 
 #Cria uma instância do framework flask
 app = Flask(__name__)
 
-#Estabelece a conexão com o banco de dados
+#Estabelece a conexão com o banco de dados.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:mysqlroot@localhost/SensorData'
 db.init_app(app)
 
-#Criar todas as tabelas no banco de dados
+#Criar todas as tabelas no banco de dados.
 with app.app_context():
     db.create_all()
 
-#Definindo a rota para renderizar o formulário HTML da página home
-@app.route('/')
-def index():
+#Função simuladora de sensores para enviar à rota que trata os payloads.
+def send_simulated_data_to_route():
+    current_time = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f%z")
 
-    # Preencher o banco de dados com dados simulados ao iniciar o aplicativo
+    #Estabelece os parâmetros de geração dos valores aleatórios dos payloads simulados.
     num_records = 2000
     equipment_id_prefix = "EQ"
-    timestamp_start = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S")
-    timestamp_end = "2024-02-01T00:00:00"
-    min_value = 10
-    max_value = 1000
+    timestamp_start ="2024-02-10T00:30:00.000+00:00"
+    timestamp_end = current_time
+    min_value = 100
+    max_value = 5000
     null_value_prob = 0.2
 
-    SensorData.fill_database_with_simulated_data(num_records, equipment_id_prefix, timestamp_start, timestamp_end, min_value, max_value, null_value_prob)
+
+    for _ in range(num_records):
+        simulated_data = SensorData.generate_simulated_data(equipment_id_prefix, timestamp_start, timestamp_end, min_value, max_value, null_value_prob)
+        response = app.test_client().post('/data', json=simulated_data)
+
+
+
+#Definindo a rota para renderizar o formulário HTML da página home.
+@app.route('/', methods=['GET', 'POST'])
+def index():
     return render_template('home.html')
 
+#Definindo a rota para a geração da tabela com registros com valor nulo.
 @app.route('/search_values')
 def search_null():
     return render_template('search_values.html', records = SensorData.search_values_byObjct())
 
-#Definindo a rota para receber o arquivo JSON
+#Definindo a rota para receber o arquivo JSON.
 @app.route('/data', methods=['POST'])
 def receive_data():
     return SensorData.receive_JSON()
 
-#Definindo a rota para receber o arquivo CSV
+#Definindo a rota para receber o arquivo CSV.
 @app.route('/upload_csv', methods=['GET', 'POST'])
 def upload_csv():
     if request.method == 'POST':
         return SensorData.receive_CSV()
     return render_template('upload_csv.html')
 
+#Definindo a rota para renderizar os gráficos.
 @app.route('/graphics', methods = ['GET'])
 def graphics():
     return render_template('graphics.html')
 
+#Definindo a rota para receber o período de tempo do usuário.
 @app.route('/get_chart', methods=['GET'])
 def get_chart():
+    #Recebe o tempo selecionado.
     chart_type = request.args.get('chartType')
 
-    # Calcula os valores médios para o período de tempo específico (24h, 48h, 1w, 1m)
+    # Calcula os valores médios para o período de tempo específico (24h, 48h, 1w, 1m).
     average_values = SensorData.calculate_average_values(chart_type)
 
-    # Retorna os valores médios como resposta JSON
+    # Retorna os valores médios como resposta JSON.
     return jsonify(average_values)
 
 
 if __name__ == '__main__':
+    send_simulated_data_to_route()
     #Para executar apenas uma vez
     if not os.environ.get("WERKZEUG_RUN_MAIN"): 
         webbrowser.open("http://127.0.0.1:5000/")
         
-app.run(debug=True)
+app.run(debug=False)
--- app/models.py
+++ app/models.py
@@ -1,54 +1,55 @@
 from datetime import datetime, timedelta
 import random
-from flask import jsonify, request
+from flask import jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import numpy as np
 import pandas as pd
 
+#Instância do  SQLAlchemy para manipulação do banco de dados.
 db = SQLAlchemy()
 
-
-#Representa a estrutura do banco de dados
+#Representa a estrutura do banco de dados.
 class SensorData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     equipmentId = db.Column(db.String(50), nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False)
     value = db.Column(db.Float(precision=2), nullable=True)
     
-    #Função para receber o request JSON
+    #Função para receber o request JSON.
     def receive_JSON():
 
+        #Identifica a requisição do payload em formato JSON.
         data = request.json
         equipmentId = data['equipmentId']
         timestamp = datetime.strptime(data['timestamp'], "%Y-%m-%dT%H:%M:%S.%f%z")
         
-        #Verifica se o campo value está presente
+        #Verifica se o campo value está presente.
         if 'value' in data:
             try:
                 value = round(float(data['value']), 2)
-            #Captura os eventuais erros de escrita ou valor e atrui o valor nulo
+            #Captura os eventuais erros de escrita ou valor e atrui o valor nulo.
             except (TypeError, ValueError):
                 value = None
-        #Caso não esteja presente, atribui o valor nulo
+        #Caso não esteja presente, atribui o valor nulo.
         else:
             value = None
 
-        #Adiciona o dado do sensor ao banco de dados
+        #Adiciona o dado do sensor ao banco de dados.
         new_sensor_data = SensorData(equipmentId=equipmentId, timestamp=timestamp, value=value)
         db.session.add(new_sensor_data)
         db.session.commit()
 
         return jsonify({'message': 'Data received!'}), 201
     
-    #Função para buscar valores do campo 'value' nulos e retornando um dicionário
+    #Função para buscar valores do campo 'value' nulos.
     def search_values_byId():
         # Consulta os registros no banco de dados com 'value' nulo
         null_value_records = SensorData.query.filter(SensorData.value.is_(None)).all()
         
         # Cria um dicionário de tuplas (equipmentId) para os registros com 'value' nulo
-        print("Registros com value nulo:", null_value_records)
         return {(record.equipmentId): record for record in null_value_records}
     
+    #Função para buscar todos os objetos (registros) com campo 'value' nulo.
     def search_values_byObjct():
         return SensorData.query.filter(SensorData.value.is_(None)).all()
 
@@ -79,28 +80,29 @@ class SensorData(db.Model):
                     value = round(float(row['value']), 2)
 
                     if (equipmentId) in map_values:
-                        # Atualiza o 'value' do registro correspondente com o valor do arquivo CSV
+                        # Atualiza o 'value' do registro correspondente com o valor do arquivo CSV.
                        record = map_values[(equipmentId)]
                        record.value = value
                        print(f"Registro atualizado: equipmentId={equipmentId}, timestamp={timestamp}, value={value}")
                     else:
-                        #Caso não haja valores correspondentes, cria um novo registro na tabela
+                        #Caso não haja valores correspondentes, cria um novo registro na tabela.
                         sensor_data = SensorData(equipmentId=equipmentId, timestamp=timestamp, value=value)
                         db.session.add(sensor_data)
                     
-                # Commit para salvar as alterações no banco de dados
+                # Commit para salvar as alterações no banco de dados.
                 db.session.commit()
                 
-                return "CSV file was processed and database was Updated", 200
+                #Retorno o template da página de informação sobre a inserção do CSV.
+                return render_template('message.html', mensagem = "CSV file was processed and database was Updated") 
             
             else:
-                return "Please, insert a CVS file", 400
+                return render_template('message.html', mensagem = "Please, insert a CVS file") 
         
         except Exception as e:
             return f"Error processing CSV file: {str(e)}", 500
 
-    @staticmethod
-    def generate_simulated_data(equipment_id_prefix, timestamp_start, timestamp_end, min_value=50.0, max_value=100.0, null_value_prob=0.2):
+    #Função para gerar os valores aleatórios no formato JSON utilizado no projeto.
+    def generate_simulated_data(equipment_id_prefix, timestamp_start, timestamp_end, min_value, max_value, null_value_prob):
             equipment_id = f"{equipment_id_prefix}-{random.randint(10000, 99999)}"
             timestamp = SensorData.generate_random_timestamp(timestamp_start, timestamp_end)
             value = round(random.uniform(min_value, max_value), 2) if random.random() > null_value_prob else None
@@ -111,34 +113,12 @@ class SensorData(db.Model):
                 "value": value
         }
 
-    @staticmethod  
+    #Função para gerar um timestamp aleatório dentro do intervalo de tempo especificado.
     def generate_random_timestamp(start_time, end_time):
-        start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S")
-        end_time = datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%S")
+        start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S.%f%z")
+        end_time = datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%S.%f%z")
         random_timestamp = start_time + (end_time - start_time) * random.random()
         return random_timestamp
-
-    @staticmethod  
-    def fill_database_with_simulated_data(num_records, equipment_id_prefix, timestamp_start, timestamp_end, min_value=50.0, max_value=100.0, null_value_prob=0.2):
-        for _ in range(num_records):
-            simulated_data = SensorData.generate_simulated_data(equipment_id_prefix, timestamp_start, timestamp_end, min_value, max_value, null_value_prob)
-            SensorData.save_simulated_data_to_database(simulated_data)
-
-        print(f"Simulação concluída. {num_records} registros foram adicionados ao banco de dados.")
-
-    @staticmethod
-    def save_simulated_data_to_database(data):
-        try:
-            equipmentId = data['equipmentId']
-            timestamp = datetime.fromisoformat(data['timestamp'])
-            value = data['value']
-
-            new_sensor_data = SensorData(equipmentId=equipmentId, timestamp=timestamp, value=value)
-            db.session.add(new_sensor_data)
-            db.session.commit()
-        except Exception as e:
-            print(f"Erro ao salvar dados simulados no banco de dados: {e}")
-
 
     #Realiza o cálculo da média dos valores do banco
     def calculate_average_values(chart_type):
@@ -157,28 +137,21 @@ class SensorData(db.Model):
             start_time = datetime.now() - timedelta(weeks=4)
         else:
             raise ValueError("Tipo de gráfico inválido")
-        
-        print(start_time)
 
-        # Consulta os registros no banco de dados para o período especificado
+        #Consulta os registros no banco de dados para o período especificado
         query_result = SensorData.query.filter(SensorData.timestamp >= start_time).all()
 
-        print(query_result)
-
-        # Extrai os valores relevantes para cálculo da média (excluindo valores None)
+        #Extrai os valores relevantes para cálculo da média (excluindo valores None)
         values = [record.value for record in query_result if record.value is not None]
 
-        print(values)
-
-        # Calcula a média dos valores
+        # Calcula a média dos valores.
         if values:
             average_value = np.mean(values)
         else:
             average_value = 0
 
+        #Atribui o chart selecionado pelo usuário à média.
         average_values[chart_type] = average_value
-
-        print(average_values)
 
         return average_values
 
